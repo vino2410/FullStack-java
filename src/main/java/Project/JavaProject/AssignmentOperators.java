4b059cd41bd2024bf91ff11e12ca473a99bf0746
vino2410 <vinojks24@gmail.com>
2022-08-13 21:57:56 +0530

    initialized git

---
+++ src/main/java/Project/JavaProject/AssignmentOperators.java
@@ -0,0 +1,25 @@
+package Project.JavaProject;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  int a = 5; 
+
+	      int b = 10; 
+
+	      int c = a+b; 
+
+	      b+=5;           //here we added extra value 5 to the variable 'b'  
+
+	      c-=10;          //here we subracted 10 in the variable 'c' 
+
+	ANS: 
+
+	        System.out.println(b); 
+
+	        System.out.println(c); 
+
+	}
+
+}
