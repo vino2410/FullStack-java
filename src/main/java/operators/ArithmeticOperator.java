d4e573473710298d45849800af128fc38ec69ecf
vino2410 <vinojks24@gmail.com>
2022-08-13 22:17:35 +0530

    second commit

---
+++ src/main/java/operators/ArithmeticOperator.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub Int a = 10; 
+
+	      int b = 5; 
+	      int a = 10;
+
+	      int c = b+a; 
+
+	      System.out.println(c);
+
+	}
+
+}
