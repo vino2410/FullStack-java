a10065b963310cbfeb759ab2ccbc3597727180f1
vino2410 <vinojks24@gmail.com>
2022-09-12 22:46:14 +0530

    Looping

---
+++ src/main/java/Looping/DoWhileloop.java
@@ -0,0 +1,17 @@
+package Looping;
+
+public class DoWhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		do {
+			System.out.println("vinod");
+			i++;
+		}
+		while(i<5);
+		
+
+	}
+
+}
